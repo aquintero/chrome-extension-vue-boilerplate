c1dd2ecef1fb6f1303a627f96a7fdf5b6b5ccb36
Alex Quintero <alex.quintero@gmail.com>
2018-03-25 20:18:17 -0400

    veutify

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -77,7 +77,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: 'file-loader',
         options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
@@ -85,7 +85,7 @@ module.exports = {
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: 'file-loader',
         options: {
           limit: 10000,
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
@@ -93,7 +93,7 @@ module.exports = {
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: 'file-loader',
         options: {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
--- src/scripts/App.vue
+++ src/scripts/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app></v-toolbar>
+    <v-content>
+    <v-container fluid>
+      <router-view></router-view>
+    </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
 <script>
